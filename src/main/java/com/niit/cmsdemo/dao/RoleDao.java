a70c34fc8d730835dd98f8f2a109480da15cbf83
jian1299 <894504231@qq.com>
2017-11-03 22:00:52 +0800

    add dao files

---
+++ src/main/java/com/niit/cmsdemo/dao/RoleDao.java
@@ -0,0 +1,8 @@
+package com.niit.cmsdemo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RoleDao {
+
+}
---
+++ src/main/java/com/niit/cmsdemo/dao/UserRoleDao.java
@@ -0,0 +1,7 @@
+package com.niit.cmsdemo.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRoleDao {
+}
