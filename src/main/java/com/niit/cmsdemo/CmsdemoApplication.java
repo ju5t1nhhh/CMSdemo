df71e7a895a001bfe5bbfc07bbbb215159134fec
Jackie2330489 <qd2017hst@163.com>
2017-11-04 16:34:01 +0800

    add MapperScan

--- src/main/java/com/niit/cmsdemo/CmsdemoApplication.java
+++ src/main/java/com/niit/cmsdemo/CmsdemoApplication.java
@@ -1,9 +1,11 @@
 package com.niit.cmsdemo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.niit.cmsdemo.dao")
 public class CmsdemoApplication {
 
 	public static void main(String[] args) {
