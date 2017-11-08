a48a5a5fb32d227166d826e94c87832040163610
Jackie2330489 <qd2017hst@163.com>
2017-11-08 10:52:40 +0800

    update PasswordHelper

--- src/main/java/com/niit/cmsdemo/util/PasswordHelper.java
+++ src/main/java/com/niit/cmsdemo/util/PasswordHelper.java
@@ -15,14 +15,15 @@ public class PasswordHelper {
         String newPassword = new SimpleHash(algorithmName, user.getPassword(),  ByteSource.Util.bytes(user.getUsername()), hashIterations).toHex();
         //String newPassword = new SimpleHash(algorithmName, user.getPassword()).toHex();
         user.setPassword(newPassword);
+    }
 
-    }
-    public static void main(String[] args) {
-        PasswordHelper passwordHelper = new PasswordHelper();
-        User user = new User();
-        user.setUsername("admin");
-        user.setPassword("admin");
-        passwordHelper.encryptPassword(user);
-        System.out.println(user);
-    }
+//    public static void main(String[] args) {
+//        PasswordHelper passwordHelper=new PasswordHelper();
+//        User user=new User();
+//        user.setUsername("admin");
+//        user.setPassword("admin");
+//        passwordHelper.encryptPassword(user);
+//        System.out.println(user.getPassword());
+//    }
+
 }
