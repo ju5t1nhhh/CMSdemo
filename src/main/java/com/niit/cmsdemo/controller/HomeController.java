4a879171d6d38da930e5978767fdc840582ed813
Jackie2330489 <qd2017hst@163.com>
2017-11-08 10:53:05 +0800

    add HomeController

--- src/main/java/com/niit/cmsdemo/controller/HomeController.java
+++ src/main/java/com/niit/cmsdemo/controller/HomeController.java
@@ -10,8 +10,8 @@ import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,22 +19,23 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value={ "/login","/"},method= RequestMethod.GET)
+
+    @GetMapping({"/login"})
     public String login(){
         return "login";
     }
 
-    @RequestMapping(value="/login",method= RequestMethod.POST)
+    @PostMapping("/login")
     public String login(HttpServletRequest request, User user, Model model){
-        if (StringUtils.isEmpty(user.getUsername()) || StringUtils.isEmpty(user.getPassword())) {
+        if (StringUtils.isEmpty(user.getLoginId()) || StringUtils.isEmpty(user.getPassword())) {
             request.setAttribute("msg", "用户名或密码不能为空！");
             return "login";
         }
         Subject subject = SecurityUtils.getSubject();
-        UsernamePasswordToken token=new UsernamePasswordToken(user.getUsername(),user.getPassword());
+        UsernamePasswordToken token=new UsernamePasswordToken(user.getLoginId(),user.getPassword());
         try {
             subject.login(token);
-            return "redirect:usersPage";
+            return "manage";
         }catch (LockedAccountException lae) {
             token.clear();
             request.setAttribute("msg", "用户已经被锁定不能登录，请与管理员联系！");
@@ -45,22 +46,8 @@ public class HomeController {
             return "login";
         }
     }
-    @RequestMapping(value={"/usersPage",""})
-    public String usersPage(){
-        return "user/users";
-    }
 
-    @RequestMapping("/rolesPage")
-    public String rolesPage(){
-        return "role/roles";
-    }
-
-    @RequestMapping("/resourcesPage")
-    public String resourcesPage(){
-        return "resources/resources";
-    }
-
-    @RequestMapping("/403")
+    @GetMapping("/403")
     public String forbidden(){
         return "403";
     }
