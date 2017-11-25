76a00c0befc0a90448f7fb3fa4933a247f9ee32c
胡斯庭 <qd2017hst@163.com>
2017-11-25 11:17:15 +0800

    new requirement,update database

--- sql/fakedata.sql
+++ sql/fakedata.sql
@@ -4,64 +4,82 @@ USE `cmsdemo`;
 INSERT INTO
 `user`
 VALUES
-('jackie233','justin','123456',1,now(),now()),
-('phil233','phil','123456',1,now(),now()),
-('john233','john','123456',1,now(),now());
+('jackie','3ef7164d1f6167cb9f2658c07d3c2f0a',now(),now()),
+('phil','3ef7164d1f6167cb9f2658c07d3c2f0a',now(),now()),
+('john','3ef7164d1f6167cb9f2658c07d3c2f0a',now(),now());
 
 #user_role
 INSERT INTO
 `user_role`
 VALUES
-('jackie233',1,now(),now()),
-('phil233',1,now(),now()),
-('john233',1,now(),now());
+('jackie',1),
+('phil',1),
+('john',1);
 
 #stu_source
 INSERT INTO
-`stu_source`(`name`,`parent_id`,`create_time`,`update_time`)
+`stu_source`(`name`)
 VALUES
-('Seminar',0,now(),now()),
-('Job Market',0,now(),now()),
-('Web',0,now(),now()),
-('Job Site',3,now(),now()),
-('NIIT WebSite',3,now(),now()),
-('College',3,now(),now()),
-('B2B',0,now(),now());
+('招聘会'),('上门咨询');
 
 #student
 INSERT INTO
-`student`(`name`,`gender`,`age`,`phone`,`student_source`,`classification`,`create_time`,`update_time`)
+`student`(`writer_id`,`name`,`gender`,`age`,`phone`,`qq_wechat`,`email`,`college`,`major`,`walkin_date`,
+`interview_note`,`marks`,`intention`,`student_source`,`classification`,`create_time`,`update_time`)
 VALUES
-('Apple','M',20,'17812345678','Web-NIIT WebSite','C',now(),now()),
-('Banana','M',21,'17812345678','Web-Job Site','B',now(),now()),
-('Orange','F',22,'17812345678','Web-Job Site','A',now(),now());
-
-#user_student
-INSERT INTO
-`user_student`
-VALUES
-('jackie233',1,now(),now()),
-('jackie233',2,now(),now()),
-('phil233',3,now(),now());
+('jackie','胡斯庭','M',20,'17812345678','704855911','qd2017hst@163.com','青岛大学','软件工程',now(),'床前明月光，疑是地上霜。',
+90,'Java','招聘会','B',now(),now()),
+('john','张振','M',23,'17854230489','328472291','qdzz2014@qq.com','青岛大学','软件工程',now(),'举头望明月，低头思故乡。',
+80,'Java','招聘会','A',now(),now()),
+('jackie','陈奕迅','M',40,'17606643176','27797875','eason2017@gmail.com','海南大学','软件工程',now(),'金拱门欢迎你',
+60,'开源','上门咨询','A',now(),now());
 
 #follow_up
 INSERT INTO
-`follow_up`(`stu_id`,`note`,`create_time`,`update_time`)
+`follow_up`(`stu_id`,`note`,`classification`,`create_time`,`update_time`)
 VALUES
-(1,'Too young too navie',now(),now()),
-(1,'Too Old too navie',now(),now()),
-(2,'Rahahrha',now(),now());
+(1,'有点想来','B',now(),now()),
+(1,'搞定签约','A',now(),now()),
+(2,'有点难搞','C',now(),now());
 
 #advertise
 INSERT INTO
 `advertise`(`user_id`,`url`,`create_time`,`update_time`)
 VALUES
-('jackie233','www.baidu.com',now(),now()),
-('jackie233','www.sohu.com',now(),now()),
-('jackie233','www.hupu.com',now(),now());
+('jackie','www.baidu.com',now(),now()),
+('jackie','www.sohu.com',now(),now()),
+('john','www.hupu.com',now(),now());
 
 #feedback
--- INSERT INTO
--- `feedback`(`stu_id`,`answer`,`create_time`,`update_time`)
--- VALUES
--- (1,'A,B,C;A,B,C,asdasdasd;',now(),now());
+INSERT INTO
+`feedback`(`stu_id`,`answer1`,`answer2`,`answer3`,`answer4`,`answer5`,`answer6`,`answer7`,`answer8`,`create_time`,`update_time`)
+VALUES
+(1,'1,2','1','1','3,4','1','1,2','2,4','我觉得ok。。。',now(),now()),
+(2,'1,2','2','3','3,4,大数据','2','1,2','3,4','我觉得其实可以。。。',now(),now()),
+(3,'3','1','3','3,人工智能','1','1,5','2,4','我真的很严格。。。',now(),now());
+
+#fbquestion
+INSERT INTO
+`fbquestion`(`location`,`details`,`type`)
+VALUES
+(1,'您对本次讲座的哪部分内容比较感兴趣？','checkbox'),
+(2,'您觉得NIIT的实训课程对您今后职业发展有所帮助吗？','radio'),
+(3,'您毕业后的就业方向定位在？','checkbox'),
+(4,'您是否愿意参加NIIT实训项目，更愿意参加哪种实训项目？','checkbox'),
+(5,'如果您原意参加NIIT实训项目，希望开始的时间是？','checkbox'),
+(6,'对于是否参加实训，您目前存在的问题是？','checkbox'),
+(7,'对于专业IT实训，您更看重的是哪些方面？','checkbox'),
+(8,'您对NIIT的建议和疑问可以写在下方？','text');
+
+#fbanswer
+INSERT INTO
+`fbanswer`(`location`,`question_id`,`details`)
+VALUES
+(1,1,'就业指导'),(1,2,'IT前景'),(1,3,'NIIT实训课程'),(1,4,'NIIT人才定制企业'),(1,99,'其他'),
+(2,1,'我很需要，非常有帮助'),(2,2,'暂时不需要，无帮助'),(2,3,'根本不需要'),(2,99,'其他'),
+(3,1,'IT职位'),(3,2,'非IT职位'),(3,3,'就业方向尚不清晰'),(3,99,'其他'),
+(4,1,'愿意'),(4,2,'考虑中'),(4,3,'VR'),(4,4,'Java+Android'),(4,5,'前端'),(4,6,'UI'),(4,7,'不愿意'),(4,99,'其他'),
+(5,1,'越快越好'),(5,2,'1个月内'),(5,3,'6个月内'),(5,4,'一年后'),(5,99,'其他'),
+(6,1,'计划考研'),(6,2,'自身是否适合职位'),(6,3,'经济压力'),(6,4,'就业担扰'),(6,5,'对IT不感兴趣'),(6,99,'其他'),
+(7,1,'师资配备'),(7,2,'实训、住宿环境'),(7,3,'就业安置服务'),(7,4,'课程内容'),(7,5,'获得证书'),(7,99,'其他'),
+(8,99,'其他');
--- sql/init.sql
+++ sql/init.sql
@@ -7,13 +7,10 @@ USE `cmsdemo`;
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`(
   `login_id` VARCHAR(20) NOT NULL COMMENT '登录ID',
-  `username` VARCHAR(20) NOT NULL COMMENT '用户名',
   `password` VARCHAR(100) NOT NULL COMMENT '密码',
-  `status` INT(4) NOT NULL DEFAULT 1 COMMENT '状态 1-开启 2-锁定',
   `create_time` DATETIME NOT NULL,
   `update_time` DATETIME NOT NULL,
   PRIMARY KEY (`login_id`),
-  KEY `user_username_INDEX` (`username`),
   KEY `user_create_time_INDEX` (`create_time`),
   KEY `user_update_time_INDEX` (`update_time`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -23,11 +20,7 @@ DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role`(
   `id` INT(4) NOT NULL AUTO_INCREMENT COMMENT '角色自增ID',
   `name` VARCHAR(20) NOT NULL COMMENT '角色名',
-  `create_time` DATETIME NOT NULL,
-  `update_time` DATETIME NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `role_create_time_INDEX` (`create_time`),
-  KEY `role_update_time_INDEX` (`update_time`)
+  PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #permission表
@@ -38,13 +31,9 @@ CREATE TABLE `permission`(
   `url` VARCHAR(100) COMMENT '权限路径 ex:/add',
   `type` INT(4) COMMENT '权限类型 1页面 2菜单 3按钮',
   `parent_id` INT(8) COMMENT '父权限ID',
-  `create_time` DATETIME NOT NULL,
-  `update_time` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
   KEY `permission_type_INDEX` (`type`),
-  KEY `permission_parent_id_INDEX` (`parent_id`),
-  KEY `permission_create_time_INDEX` (`create_time`),
-  KEY `permission_update_time_INDEX` (`update_time`)
+  KEY `permission_parent_id_INDEX` (`parent_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #user_role表
@@ -52,11 +41,7 @@ DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role`(
   `user_id` VARCHAR(20) NOT NULL COMMENT '用户ID',
   `role_id` INT(4) NOT NULL COMMENT '角色ID',
-  `create_time` DATETIME NOT NULL,
-  `update_time` DATETIME NOT NULL,
-  PRIMARY KEY (`user_id`,`role_id`),
-  KEY `user_role_create_time_INDEX` (`create_time`),
-  KEY `user_role_update_time_INDEX` (`update_time`)
+  PRIMARY KEY (`user_id`,`role_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #role_permission表
@@ -64,11 +49,7 @@ DROP TABLE IF EXISTS `role_permission`;
 CREATE TABLE `role_permission`(
   `role_id` INT(4) NOT NULL COMMENT '角色ID',
   `perm_id` INT(8) NOT NULL COMMENT '权限ID',
-  `create_time` DATETIME NOT NULL,
-  `update_time` DATETIME NOT NULL,
-  PRIMARY KEY (`role_id`,`perm_id`),
-  KEY `role_permission_create_time_INDEX` (`create_time`),
-  KEY `role_permission_update_time_INDEX` (`update_time`)
+  PRIMARY KEY (`role_id`,`perm_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #advertise表
@@ -89,7 +70,8 @@ CREATE TABLE `advertise`(
 DROP TABLE IF EXISTS `student`;
 CREATE TABLE `student`(
   `id` INT(12) NOT NULL AUTO_INCREMENT COMMENT '学生自增ID',
-  `name` VARCHAR(20) NOT NULL COMMENT '角色ID',
+  `writer_id` VARCHAR(20) NOT NULL COMMENT '录入人ID',
+  `name` VARCHAR(20) NOT NULL COMMENT '学生姓名',
   `gender` CHAR COMMENT '性别',
   `age` INT(4) COMMENT '年龄',
   `phone` VARCHAR(15) COMMENT '电话号码',
@@ -102,10 +84,11 @@ CREATE TABLE `student`(
   `marks` INT(4) COMMENT '笔试成绩',
   `intention` VARCHAR(100) COMMENT '学习意愿',
   `student_source` VARCHAR(100) COMMENT '学生来源',
-  `classification` CHAR COMMENT '学生分类 A,B,C',
+  `classification` CHAR COMMENT '学生初面分类 A,B,C',
   `create_time` DATETIME NOT NULL,
   `update_time` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
+  KEY `student_writer_id_INDEX` (`writer_id`),
   KEY `student_name_INDEX` (`name`),
   KEY `student_age_INDEX` (`age`),
   KEY `student_college_INDEX` (`college`),
@@ -124,24 +107,14 @@ CREATE TABLE `follow_up`(
   `id` INT(12) NOT NULL AUTO_INCREMENT COMMENT '跟进ID',
   `stu_id` INT(12) NOT NULL COMMENT '学生ID',
   `note` TEXT COMMENT '记录',
+  `classification` CHAR COMMENT '跟进状态分类',
   `create_time` DATETIME NOT NULL,
   `update_time` DATETIME NOT NULL,
   PRIMARY KEY (`id`),
   KEY `follow_up_stu_id_INDEX` (`stu_id`),
+  KEY `follow_up_classification_INDEX` (`classification`),
   KEY `follow_up_create_time_INDEX` (`create_time`),
   KEY `follow_up_update_time_INDEX` (`update_time`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-#user_student表
-DROP TABLE IF EXISTS `user_student`;
-CREATE TABLE `user_student`(
-  `user_id` VARCHAR(20) NOT NULL COMMENT '用户ID',
-  `stu_id` INT(8) NOT NULL COMMENT '学生ID',
-  `create_time` DATETIME NOT NULL,
-  `update_time` DATETIME NOT NULL,
-  PRIMARY KEY (`user_id`,`stu_id`),
-  KEY `user_student_create_time_INDEX` (`create_time`),
-  KEY `user_student_update_time_INDEX` (`update_time`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #stu_source表
@@ -149,13 +122,7 @@ DROP TABLE IF EXISTS `stu_source`;
 CREATE TABLE `stu_source`(
   `id` INT(4) NOT NULL AUTO_INCREMENT COMMENT '生源自增ID',
   `name` VARCHAR(20) NOT NULL COMMENT '生源描述',
-  `parent_id` INT(4) COMMENT '父ID',
-  `create_time` DATETIME NOT NULL,
-  `update_time` DATETIME NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `stu_source_parent_id_INDEX` (`parent_id`),
-  KEY `stu_source_create_time_INDEX` (`create_time`),
-  KEY `stu_source_update_time_INDEX` (`update_time`)
+  PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #feedback表
@@ -169,9 +136,33 @@ CREATE TABLE `feedback`(
   `answer5` TEXT COMMENT '回答5',
   `answer6` TEXT COMMENT '回答6',
   `answer7` TEXT COMMENT '回答7',
+  `answer8` TEXT COMMENT '回答8',
   `create_time` DATETIME NOT NULL,
   `update_time` DATETIME NOT NULL,
   PRIMARY KEY (`stu_id`),
-  KEY `stu_source_create_time_INDEX` (`create_time`),
-  KEY `stu_source_update_time_INDEX` (`update_time`)
+  KEY `feedback_create_time_INDEX` (`create_time`),
+  KEY `feedback_update_time_INDEX` (`update_time`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#question表
+DROP TABLE IF EXISTS `fbquestion`;
+CREATE TABLE `fbquestion`(
+  `id` INT(4) NOT NULL AUTO_INCREMENT COMMENT '问题自增ID',
+  `location` INT(4) COMMENT '位置',
+  `details` VARCHAR(200) COMMENT '问题描述',
+  `type` VARCHAR(10) COMMENT '类型 checkbox radio',
+  PRIMARY KEY (`id`),
+  KEY `fbquestion_location_INDEX` (`location`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#answer表
+DROP TABLE IF EXISTS `fbanswer`;
+CREATE TABLE `fbanswer`(
+  `id` INT(4) NOT NULL AUTO_INCREMENT COMMENT '答案自增ID',
+  `question_id` INT(4) COMMENT '问题id',
+  `location` INT(4) COMMENT '位置',
+  `details` VARCHAR(200) COMMENT '答案描述',
+  PRIMARY KEY (`id`),
+  KEY `fbanswer_location_INDEX` (`location`),
+  KEY `fbanswer_question_id_INDEX` (`question_id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
