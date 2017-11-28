166a23178bd900fb381d0f30d0d01c47bc3bbaf9
胡斯庭 <qd2017hst@163.com>
2017-11-28 16:40:44 +0800

    增加问题上限到20

--- src/main/java/com/niit/cmsdemo/domain/Feedback.java
+++ src/main/java/com/niit/cmsdemo/domain/Feedback.java
@@ -20,8 +20,48 @@ public class Feedback implements Serializable {
     private String answer9;
     private String answer10;
     private String answer11;
+    private String answer12;
+    private String answer13;
+    private String answer14;
+    private String answer15;
+    private String answer16;
+    private String answer17;
+    private String answer18;
+    private String answer19;
+    private String answer20;
+    private String answer21;
     private Date createTime;
     private Date updateTime;
+
+    @Override
+    public String toString() {
+        return "Feedback{" +
+                "stuId=" + stuId +
+                ", answer1='" + answer1 + '\'' +
+                ", answer2='" + answer2 + '\'' +
+                ", answer3='" + answer3 + '\'' +
+                ", answer4='" + answer4 + '\'' +
+                ", answer5='" + answer5 + '\'' +
+                ", answer6='" + answer6 + '\'' +
+                ", answer7='" + answer7 + '\'' +
+                ", answer8='" + answer8 + '\'' +
+                ", answer9='" + answer9 + '\'' +
+                ", answer10='" + answer10 + '\'' +
+                ", answer11='" + answer11 + '\'' +
+                ", answer12='" + answer12 + '\'' +
+                ", answer13='" + answer13 + '\'' +
+                ", answer14='" + answer14 + '\'' +
+                ", answer15='" + answer15 + '\'' +
+                ", answer16='" + answer16 + '\'' +
+                ", answer17='" + answer17 + '\'' +
+                ", answer18='" + answer18 + '\'' +
+                ", answer19='" + answer19 + '\'' +
+                ", answer20='" + answer20 + '\'' +
+                ", answer21='" + answer21 + '\'' +
+                ", createTime=" + createTime +
+                ", updateTime=" + updateTime +
+                '}';
+    }
 
     public static long getSerialVersionUID() {
         return serialVersionUID;
@@ -123,6 +163,86 @@ public class Feedback implements Serializable {
         this.answer11 = answer11;
     }
 
+    public String getAnswer12() {
+        return answer12;
+    }
+
+    public void setAnswer12(String answer12) {
+        this.answer12 = answer12;
+    }
+
+    public String getAnswer13() {
+        return answer13;
+    }
+
+    public void setAnswer13(String answer13) {
+        this.answer13 = answer13;
+    }
+
+    public String getAnswer14() {
+        return answer14;
+    }
+
+    public void setAnswer14(String answer14) {
+        this.answer14 = answer14;
+    }
+
+    public String getAnswer15() {
+        return answer15;
+    }
+
+    public void setAnswer15(String answer15) {
+        this.answer15 = answer15;
+    }
+
+    public String getAnswer16() {
+        return answer16;
+    }
+
+    public void setAnswer16(String answer16) {
+        this.answer16 = answer16;
+    }
+
+    public String getAnswer17() {
+        return answer17;
+    }
+
+    public void setAnswer17(String answer17) {
+        this.answer17 = answer17;
+    }
+
+    public String getAnswer18() {
+        return answer18;
+    }
+
+    public void setAnswer18(String answer18) {
+        this.answer18 = answer18;
+    }
+
+    public String getAnswer19() {
+        return answer19;
+    }
+
+    public void setAnswer19(String answer19) {
+        this.answer19 = answer19;
+    }
+
+    public String getAnswer20() {
+        return answer20;
+    }
+
+    public void setAnswer20(String answer20) {
+        this.answer20 = answer20;
+    }
+
+    public String getAnswer21() {
+        return answer21;
+    }
+
+    public void setAnswer21(String answer21) {
+        this.answer21 = answer21;
+    }
+
     public Date getCreateTime() {
         return createTime;
     }
@@ -137,25 +257,5 @@ public class Feedback implements Serializable {
 
     public void setUpdateTime(Date updateTime) {
         this.updateTime = updateTime;
-    }
-
-    @Override
-    public String toString() {
-        return "Feedback{" +
-                "stuId=" + stuId +
-                ", answer1='" + answer1 + '\'' +
-                ", answer2='" + answer2 + '\'' +
-                ", answer3='" + answer3 + '\'' +
-                ", answer4='" + answer4 + '\'' +
-                ", answer5='" + answer5 + '\'' +
-                ", answer6='" + answer6 + '\'' +
-                ", answer7='" + answer7 + '\'' +
-                ", answer8='" + answer8 + '\'' +
-                ", answer9='" + answer9 + '\'' +
-                ", answer10='" + answer10 + '\'' +
-                ", answer11='" + answer11 + '\'' +
-                ", createTime=" + createTime +
-                ", updateTime=" + updateTime +
-                '}';
     }
 }
