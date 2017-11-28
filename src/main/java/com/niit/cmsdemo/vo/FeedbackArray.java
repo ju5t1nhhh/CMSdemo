56a99de56e4a0dfaa6827b48c237405063cf3033
胡斯庭 <qd2017hst@163.com>
2017-11-28 16:42:04 +0800

    添加转换类 为筛选功能

--- src/main/java/com/niit/cmsdemo/vo/FeedbackArray.java
+++ src/main/java/com/niit/cmsdemo/vo/FeedbackArray.java
@@ -13,6 +13,42 @@ public class FeedbackArray {
     private List<String> answer8;
     private List<String> answer9;
     private List<String> answer10;
+    private List<String> answer11;
+    private List<String> answer12;
+    private List<String> answer13;
+    private List<String> answer14;
+    private List<String> answer15;
+    private List<String> answer16;
+    private List<String> answer17;
+    private List<String> answer18;
+    private List<String> answer19;
+    private List<String> answer20;
+
+    @Override
+    public String toString() {
+        return "FeedbackArray{" +
+                "answer1=" + answer1 +
+                ", answer2=" + answer2 +
+                ", answer3=" + answer3 +
+                ", answer4=" + answer4 +
+                ", answer5=" + answer5 +
+                ", answer6=" + answer6 +
+                ", answer7=" + answer7 +
+                ", answer8=" + answer8 +
+                ", answer9=" + answer9 +
+                ", answer10=" + answer10 +
+                ", answer11=" + answer11 +
+                ", answer12=" + answer12 +
+                ", answer13=" + answer13 +
+                ", answer14=" + answer14 +
+                ", answer15=" + answer15 +
+                ", answer16=" + answer16 +
+                ", answer17=" + answer17 +
+                ", answer18=" + answer18 +
+                ", answer19=" + answer19 +
+                ", answer20=" + answer20 +
+                '}';
+    }
 
     public List<String> getAnswer1() {
         return answer1;
@@ -94,19 +130,83 @@ public class FeedbackArray {
         this.answer10 = answer10;
     }
 
-    @Override
-    public String toString() {
-        return "FeedbackArray{" +
-                "answer1=" + answer1 +
-                ", answer2=" + answer2 +
-                ", answer3=" + answer3 +
-                ", answer4=" + answer4 +
-                ", answer5=" + answer5 +
-                ", answer6=" + answer6 +
-                ", answer7=" + answer7 +
-                ", answer8=" + answer8 +
-                ", answer9=" + answer9 +
-                ", answer10=" + answer10 +
-                '}';
+    public List<String> getAnswer11() {
+        return answer11;
+    }
+
+    public void setAnswer11(List<String> answer11) {
+        this.answer11 = answer11;
+    }
+
+    public List<String> getAnswer12() {
+        return answer12;
+    }
+
+    public void setAnswer12(List<String> answer12) {
+        this.answer12 = answer12;
+    }
+
+    public List<String> getAnswer13() {
+        return answer13;
+    }
+
+    public void setAnswer13(List<String> answer13) {
+        this.answer13 = answer13;
+    }
+
+    public List<String> getAnswer14() {
+        return answer14;
+    }
+
+    public void setAnswer14(List<String> answer14) {
+        this.answer14 = answer14;
+    }
+
+    public List<String> getAnswer15() {
+        return answer15;
+    }
+
+    public void setAnswer15(List<String> answer15) {
+        this.answer15 = answer15;
+    }
+
+    public List<String> getAnswer16() {
+        return answer16;
+    }
+
+    public void setAnswer16(List<String> answer16) {
+        this.answer16 = answer16;
+    }
+
+    public List<String> getAnswer17() {
+        return answer17;
+    }
+
+    public void setAnswer17(List<String> answer17) {
+        this.answer17 = answer17;
+    }
+
+    public List<String> getAnswer18() {
+        return answer18;
+    }
+
+    public void setAnswer18(List<String> answer18) {
+        this.answer18 = answer18;
+    }
+
+    public List<String> getAnswer19() {
+        return answer19;
+    }
+
+    public void setAnswer19(List<String> answer19) {
+        this.answer19 = answer19;
+    }
+
+    public List<String> getAnswer20() {
+        return answer20;
+    }
+
+    public void setAnswer20(List<String> answer20) {
+        this.answer20 = answer20;
     }
 }
